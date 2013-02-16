9b11f877a9ae6653b001f8e7b1254c428a537b30
rolandkrueger <info@rolandkrueger.info>
2013-02-16 12:57:21 +0100

    Added Readme, added comments.

---
+++ en/architecture/UsingSessionAndUIScope/README.md
@@ -0,0 +1,26 @@
+Vaadin’s Variable Scopes: VaadinSession and UI
+==============================================================
+
+Abstract
+--------
+
+
+- - - - - - - - -
+
+* __Tutorial__: [Vaadin’s Variable Scopes: VaadinSession and UI](http://blog.oio.de)
+
+* __Author__: Roland Krüger
+
+* __Publishing Date__: February xxth, 2013
+
+* __Tutorial Language__: English
+
+* __Vaadin Version__: 7.0.0 or higher
+
+Build
+-----
+
+To run the example, [Apache Maven](http://maven.apache.org) is needed. 
+
+- - - - - - - - - -
+This project is hosted on GitHub: https://github.com/rolandkrueger/vaadin-by-example
--- en/architecture/UsingSessionAndUIScope/src/main/java/de/oio/vaadin/MainApplicationUI.java
+++ en/architecture/UsingSessionAndUIScope/src/main/java/de/oio/vaadin/MainApplicationUI.java
@@ -16,7 +16,17 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-// If the user hits F5 (browser refresh) a new UI object shall not be created
+/**
+ * <p>
+ * This demo application demonstrates the two different scopes of a Vaadin 7
+ * session. These two scopes are the common session scope and the new UI scope.
+ * The UI scope represents the data needed for the contents of one browser
+ * window or tab from the same session.
+ * </p>
+ * 
+ */
+// We're using @PreserveOnRefresh so that no new UI object will be created if
+// the user reloads the pages.
 @PreserveOnRefresh
 public class MainApplicationUI extends UI {
 	private final static String SESSION_SCOPED_VALUE_ID = "sessionScopedValue";
@@ -37,25 +47,25 @@ public class MainApplicationUI extends UI {
 		layout.setMargin(true);
 
 		String headlineText = "<h1>Using Vaadin's Session and UI Scope</h1><hr/>";
-		Label heading = new Label(headlineText, ContentMode.HTML);
+		layout.addComponent(new Label(headlineText, ContentMode.HTML));
 
-		layout.addComponent(heading);
-
+		// we add a label that contains a link to the demo application itself.
+		// When this link is opened in a new browser window or tab, a new UI
+		// object for this window will be created by the framework. All field
+		// variables of this class live in this new UI object and are thus
+		// multiplied in memory for each new UI object.
 		layout.addComponent(new Label(
 				"Open this application in a new browser window/tab <a href=\"#\" target=\"_blank\">with this link</a> and"
-						+ " refresh the browser contents after having edited the scoped variables.",
-				ContentMode.HTML));
+						+ " refresh the browser contents after having edited the scoped variables.", ContentMode.HTML));
 
-		MainApplicationUI.getCurrentUIScopedVariable().addValueChangeListener(
-				new ValueChangeListener() {
-					@Override
-					public void valueChange(ValueChangeEvent event) {
-						refreshOverviewTable();
-					}
-				});
+		MainApplicationUI.getCurrentUIScopedVariable().addValueChangeListener(new ValueChangeListener() {
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				refreshOverviewTable();
+			}
+		});
 
-		layout.addComponent(new Label("Current UI's ID: "
-				+ String.valueOf(UI.getCurrent().getUIId())));
+		layout.addComponent(new Label("Current UI's ID: " + String.valueOf(UI.getCurrent().getUIId())));
 
 		layout.addComponent(createFormLayout());
 		layout.addComponent(new Label(overviewTableProperty, ContentMode.HTML));
@@ -72,6 +82,10 @@ public class MainApplicationUI extends UI {
 		setContent(layout);
 	}
 
+	public static MainApplicationUI getCurrent() {
+		return (MainApplicationUI) UI.getCurrent();
+	}
+
 	private void initUIScopedVariable() {
 		uiScopedVariable = new ObjectProperty<String>("");
 		overviewTableProperty = new ObjectProperty<String>("");
@@ -79,12 +93,10 @@ public class MainApplicationUI extends UI {
 	}
 
 	private void initSessionScopedVariable() {
-		if (getSessionScopedVariable() == null) {
+		if (MainApplicationUI.getSessionScopedVariable() == null) {
 			try {
 				VaadinSession.getCurrent().getLockInstance().lock();
-				VaadinSession.getCurrent()
-						.setAttribute(SESSION_SCOPED_VALUE_ID,
-								new ObjectProperty<String>(""));
+				VaadinSession.getCurrent().setAttribute(SESSION_SCOPED_VALUE_ID, new ObjectProperty<String>(""));
 			} finally {
 				VaadinSession.getCurrent().getLockInstance().unlock();
 			}
@@ -92,9 +104,8 @@ public class MainApplicationUI extends UI {
 	}
 
 	@SuppressWarnings("unchecked")
-	private Property<String> getSessionScopedVariable() {
-		Object value = VaadinSession.getCurrent().getAttribute(
-				SESSION_SCOPED_VALUE_ID);
+	public static Property<String> getSessionScopedVariable() {
+		Object value = VaadinSession.getCurrent().getAttribute(SESSION_SCOPED_VALUE_ID);
 		return value == null ? null : (Property<String>) value;
 	}
 
@@ -103,16 +114,14 @@ public class MainApplicationUI extends UI {
 	}
 
 	public static ObjectProperty<String> getCurrentUIScopedVariable() {
-		return ((MainApplicationUI) UI.getCurrent()).getUIScopedVariable();
+		return MainApplicationUI.getCurrent().getUIScopedVariable();
 	}
 
 	private FormLayout createFormLayout() {
 		FormLayout formLayout = new FormLayout();
 		formLayout.setCaption("Edit session/UI scoped variables:");
-		uiScopedValueTF = new TextField("UI-scoped value:",
-				MainApplicationUI.getCurrentUIScopedVariable());
-		sessionScopedValueTF = new TextField("Session-scoped value:",
-				getSessionScopedVariable());
+		uiScopedValueTF = new TextField("UI-scoped value:", MainApplicationUI.getCurrentUIScopedVariable());
+		sessionScopedValueTF = new TextField("Session-scoped value:", getSessionScopedVariable());
 
 		uiScopedValueTF.setImmediate(true);
 		sessionScopedValueTF.setImmediate(true);
@@ -126,19 +135,16 @@ public class MainApplicationUI extends UI {
 	private void refreshOverviewTable() {
 		StringBuilder buf = new StringBuilder();
 		buf.append("<h2>Overview of all UI scoped variables in the current session</h2>");
-		buf.append("There are currently ")
-				.append(VaadinSession.getCurrent().getUIs().size())
+		buf.append("There are currently ").append(VaadinSession.getCurrent().getUIs().size())
 				.append(" UI objects active for this session (Session ID <em>");
-		buf.append(VaadinSession.getCurrent().getSession().getId()).append(
-				"</em>).");
+		buf.append(VaadinSession.getCurrent().getSession().getId()).append("</em>).");
 		buf.append("<table>");
 
 		for (UI ui : VaadinSession.getCurrent().getUIs()) {
 			buf.append("<tr><td>");
 			buf.append("UI ID ").append(ui.getUIId()).append(": ");
 			buf.append("</td><td>");
-			buf.append(((MainApplicationUI) ui).getUIScopedVariable()
-					.getValue());
+			buf.append(((MainApplicationUI) ui).getUIScopedVariable().getValue());
 			buf.append("</td></tr>");
 		}
 
