acc263959faadc023f3ce6d4f801080b3628c918
rolandkrueger <info@rolandkrueger.info>
2013-03-11 09:43:10 +0100

    Updated Vaadin version from 7.0.0.alpha3 to 7.0.1

--- en/build-and-development/DynamicReloadingWithMavenAndEclipse/src/main/java/de/oio/blog/vaadin/maven/MyApplication.java
+++ en/build-and-development/DynamicReloadingWithMavenAndEclipse/src/main/java/de/oio/blog/vaadin/maven/MyApplication.java
@@ -1,15 +1,18 @@
 package de.oio.blog.vaadin.maven;
 
-import com.vaadin.terminal.WrappedRequest;
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Root;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
-public class MyApplication extends Root {
+public class MyApplication extends UI {
   @Override
-  public void init(WrappedRequest request) {
+  public void init(VaadinRequest request) {
+    VerticalLayout layout = new VerticalLayout();
+
     Label label = new Label(
         "Hello Vaadin user! Change this text while running 'mvn jetty:run-exploded' and recompile with 'mvn compile -Pdevelopment'.");
-    addComponent(label);
+    layout.addComponent(label);
+    setContent(layout);
   }
-
 }
