b8bddec62dc06cfe8dff57029de5a59face171be
Roland Krüger <info@rolandkrueger.info>
2014-04-20 14:04:46 +0200

    Revert "deletions for i18n demo package renaming"
    This reverts commit 7be6ce7d9d368c72420c2f168b74f36b7c9c73d4.

---
+++ all-in-one-demo/src/main/java/de/oio/vaadin/demo/i18nforcustomlayoutsusingvelocity/I18nForCustomLayoutsUsingVelocityView.java
@@ -0,0 +1,11 @@
+package de.oio.vaadin.demo.i18nforcustomlayoutsusingvelocity;
+
+import org.vaadin.appbase.components.TranslatedCustomLayout;
+
+public class I18nForCustomLayoutsUsingVelocityView extends TranslatedCustomLayout {
+
+	public I18nForCustomLayoutsUsingVelocityView() {
+		super("demos/i18nForCustomLayoutsUsingVelocity");
+	}
+
+}
