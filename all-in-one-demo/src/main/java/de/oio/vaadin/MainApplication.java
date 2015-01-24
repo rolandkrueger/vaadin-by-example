fb99d104b501a35e7834a4452189be3f8305c49c
rolandkrueger <info@rolandkrueger.info>
2015-01-24 15:44:06 +0100

    Configure Spring Boot war deployment

--- all-in-one-demo/src/main/java/de/oio/vaadin/MainApplication.java
+++ all-in-one-demo/src/main/java/de/oio/vaadin/MainApplication.java
@@ -2,11 +2,12 @@ package de.oio.vaadin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"de.oio.vaadin", "org.vaadin.appbase"})
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(new Class[]{MainApplication.class}, args);
     }
